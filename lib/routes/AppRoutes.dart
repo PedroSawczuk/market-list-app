8c1e96f6392c7d92d27518429ca64f4467fa5c9c
PedroSawczuk <pedrosawczuk53@gmail.com>
2024-07-26 18:02:18 -0400

    Atualizando rotas

--- lib/routes/AppRoutes.dart
+++ lib/routes/AppRoutes.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import '../screens/HomePage.dart';
+import '../screens/authentication/SignUpPage.dart';
+import '../screens/authentication/LoginPage.dart';
+import '../screens/authentication/ForgotPasswordPage.dart';
+import '../screens/developerPage.dart';
 
 class AppRoutes {
-  static const homePage = 'homePage';
+  static const homePage = '/homePage';
+  static const signUpPage = '/signUpPage';
+  static const loginPage = '/loginPage';
+  static const forgotPasswordPage = '/forgotPasswordPage';
+  static const developerPage = '/developerPage';
 
   static Map<String, WidgetBuilder> define() {
     return {
       homePage: (BuildContext context) => HomePage(),
+      signUpPage: (BuildContext context) => SignUpPage(),
+      loginPage: (BuildContext context) => LoginPage(),
+      forgotPasswordPage: (BuildContext context) => ForgotPasswordPage(),
+      developerPage: (BuildContext context) => DeveloperPage(),
     };
   }
 }
