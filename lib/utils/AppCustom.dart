32b64c743f8848e4a850eed3fdd3f353b81f873c
PedroSawczuk <pedrosawczuk53@gmail.com>
2024-07-26 18:01:56 -0400

    Atualizando as utilizades

--- lib/utils/AppCustom.dart
+++ lib/utils/AppCustom.dart
@@ -4,4 +4,10 @@ class AppCustom {
   static const colorWhite = Colors.white;
   static const colorBlack = Color(0xFF1B1A1A);
   static const colorOrange = Color(0xFFD86A03);
+  static const colorBlue = Color(0xFF0961E4);
+
+  static const SB10 = SizedBox(height: 10);
+  static const SB20 = SizedBox(height: 20);
+  static const SB30 = SizedBox(height: 30);
+  static const SB40 = SizedBox(height: 40);
 }
--- lib/utils/DrawerCustom.dart
+++ lib/utils/DrawerCustom.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:market_list_app/routes/AppRoutes.dart';
 import '../utils/AppCustom.dart';
 
 class DrawerCustom extends StatelessWidget {
@@ -25,9 +26,16 @@ class DrawerCustom extends StatelessWidget {
           ListTile(
             leading: Icon(Icons.home),
             onTap: () {
-              // Add Function
+              Navigator.pushNamed(context, AppRoutes.homePage);
             },
             title: Text('Página Inicial'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_cart_rounded),
+            onTap: () {
+              // Add Function
+            },
+            title: Text('Compras'),
           ),
           ListTile(
             leading: Icon(Icons.person),
@@ -39,7 +47,8 @@ class DrawerCustom extends StatelessWidget {
           ListTile(
             leading: Icon(Icons.developer_mode),
             onTap: () {
-              // Add Function
+              print('Entrnado na página de Desenvolvedor');
+              Navigator.pushNamed(context, AppRoutes.developerPage);
             },
             title: Text('Desenvolvedor'),
           ),
